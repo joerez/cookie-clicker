e0fe84027271f79dbe78f5ce85d2d2d8bb98825c
Joerez <rezenbiz@gmail.com>
2018-06-18 11:02:08 -0700

    finished product

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
+//mutable variable declerations
+let grandmaInt;
+let facilityInt;
+
 //set auto clicks default values
 let autoGrandma = false;
+let autoFacility = false;
 
 //set default values
 let cookieCount = 0;
 let clickPower = 1;
-let grandmaPower = 10;
+let grandmaPower = 0;
+let facilityPower = 0;
 
 //set default amounts
 let powerClickAmount = 1;
 let grandmaAmount = 0;
+let facilityAmount = 0;
 
 //set default prices
 let powerClickPrice = 50;
 let grandmaPrice = 2000;
-let cookieManufacturingFacilityPrice = 100000;
+let facilityPrice = 100000;
 
 //delcare dom variables
 let counter = document.getElementById('counter');
 let clicker = document.getElementById('clicker');
 let buyPower = document.getElementById('buyPower');
 let buyGrandma = document.getElementById('buyGrandma');
-
+let buyFacility = document.getElementById('buyFacility');
 
 //big click button
 clicker.addEventListener("click", function() {
@@ -28,7 +35,11 @@ clicker.addEventListener("click", function() {
   refreshCookieCount();
 })
 
+/***********************************
 
+Buy functions
+
+************************************/
 
 //Buy PowerClick Function
 buyPower.addEventListener("click", function() {
@@ -40,17 +51,27 @@ buyPower.addEventListener("click", function() {
 
     //Make the price for the next level higher
     powerClickPrice = Math.floor(powerClickPrice * 1.33);
+    powerClickAmount += 1;
 
-    //Scale the price and the click amount a little bit.
-    if (powerClickAmount <= 5) {
-      clickPower = clickPower + 1;
-    } else if (powerClickAmount >= 5) {
-      clickPower = clickPower + 2;
-    } else if (powerClickAmount >= 10) {
-      clickPower = clickPower + 4;
+    //Scale click amount a little bit.
+    switch(true) {
+      case powerClickAmount <= 4:
+        clickPower = clickPower += 1;
+        break;
+      case powerClickAmount >= 5 && powerClickAmount < 10:
+        clickPower = clickPower + 2;
+        break;
+      case powerClickAmount >= 10 && powerClickAmount < 15:
+        clickPower = clickPower + 4;
+        break;
+      case powerClickAmount >= 15 && powerClickAmount < 20:
+        clickPower = clickPower + 6;
+        break;
+      case powerClickAmount >= 20:
+        clickPower = clickPower + 20;
+        break;
     }
 
-    powerClickAmount += 1;
     refreshPowerClick();
 
     //cant afford it yet
@@ -59,10 +80,12 @@ buyPower.addEventListener("click", function() {
   }
 })
 
-
-
 //Buy grandma Function
 buyGrandma.addEventListener("click", function() {
+
+  window.clearInterval(grandmaInt);
+
+  refreshGrandma();
   //if you can afford it
   if (cookieCount >= grandmaPrice) {
     //subtract cookies from the price of the item
@@ -72,10 +95,27 @@ buyGrandma.addEventListener("click", function() {
     //Make the price for the next level higher
     grandmaPrice = Math.floor(grandmaPrice * 1.33);
 
-
-    grandmaPower += 12;
-
     grandmaAmount += 1;
+
+    //Scale the price and the click amount a little bit.
+    switch(true) {
+      case grandmaAmount <= 4:
+        grandmaPower = grandmaPower += 15;
+        break;
+      case grandmaAmount >= 5 - 1 && grandmaAmount < 10:
+        grandmaPower = grandmaPower + 22;
+        break;
+      case grandmaAmount >= 10 && grandmaAmount < 15:
+        grandmaPower = grandmaPower + 40;
+        break;
+      case grandmaAmount >= 15 && grandmaAmount < 20:
+        grandmaPower = grandmaPower + 100;
+        break;
+      case grandmaAmount >= 20:
+        grandmaPower = grandmaPower + 500;
+        break;
+    }
+
     autoGrandma = true;
     refreshGrandma();
 
@@ -85,9 +125,56 @@ buyGrandma.addEventListener("click", function() {
   }
 })
 
+//Buy facility Function
+buyFacility.addEventListener("click", function() {
 
+  window.clearInterval(facilityInt);
 
+  refreshFacility();
+  //if you can afford it
+  if (cookieCount >= facilityPrice) {
+    //subtract cookies from the price of the item
+    cookieCount +=  -facilityPrice;
+    refreshCookieCount();
 
+    //Make the price for the next level higher
+    facilityPrice = Math.floor(facilityPrice * 1.33);
+
+    facilityAmount += 1;
+
+    //Scale the price and the click amount a little bit.
+    switch(true) {
+      case facilityAmount <= 4:
+        facilityPower = facilityPower += 2000;
+        break;
+      case facilityAmount >= 5 - 1 && facilityAmount < 10:
+        facilityPower = facilityPower + 2560;
+        break;
+      case facilityAmount >= 10 && facilityAmount < 15:
+        facilityPower = facilityPower + 3080;
+        break;
+      case facilityAmount >= 15 && facilityAmount < 20:
+        facilityPower = facilityPower + 3860;
+        break;
+      case FacilityAmount >= 20:
+        facilityPower = facilityPower + 6000;
+        break;
+    }
+
+    autoFacility = true;
+    refreshFacility();
+
+    //cant afford it yet
+  } else {
+    console.log('you dont have enough cookies');
+  }
+})
+
+/***********************************
+
+Refresh functions
+
+************************************/
 
 function refreshCookieCount() {
   counter.innerHTML = cookieCount;
@@ -107,16 +194,34 @@ function refreshGrandma() {
   if (autoGrandma = true) {
     grandmaAutoGo();
   }
-
-
 }
 
+function refreshFacility() {
+  document.getElementById('facilityPrice').innerHTML = facilityPrice;
+  document.getElementById('facilityCount').innerHTML = facilityAmount;
+  document.getElementById('facilityPerSec').innerHTML = facilityPower;
+
+  if (autoFacility = true) {
+    facilityAutoGo();
+  }
+}
+
+/***********************************
+
+Auto loop functions
+
+************************************/
+
 function grandmaAutoGo() {
+  let grandmaInt = window.setInterval(function(){
+      cookieCount = cookieCount+= grandmaPower / 2;
+      refreshCookieCount();
+    }, 1000);
+}
 
-  window.setInterval(function(){
-    cookieCount = cookieCount+= grandmaPower;
-    refreshCookieCount();
-  }, 1000);
-
-
+function facilityAutoGo() {
+  let facilityInt = window.setInterval(function(){
+      cookieCount = cookieCount+= facilityPower / 2;
+      refreshCookieCount();
+    }, 1000);
 }
